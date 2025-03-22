9a22ec8934ef05c05184d19f3d2f9e7651f33ca2
WahidMubarrat <wahidmubarrat20@gmail.com>
2025-03-22 16:59:14 +0600

    BMIBMRTestcase

---
+++ src/Testing/BMIBMRTest.java
@@ -0,0 +1,33 @@
+package Testing;
+import MainProgramme.BMIBMRcalculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+
+
+    class BMIBMRTest {
+
+        @Test
+        void testBMICalculation() {
+            BMIBMRcalculator calculator = new BMIBMRcalculator(100,2, 25, "male");
+            assertEquals(25, calculator.calculateBMI(), 0.01);
+        }
+
+        @Test
+        void testBMRCalculation() {
+            BMIBMRcalculator calculator = new BMIBMRcalculator(100, 2, 25, "male");
+            assertNotEquals(2100, calculator.calculateBMR(), 0.01);
+        }
+
+        @Test
+        void testInvalidGender() {
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                new BMIBMRcalculator(100, 2, 25, "other").calculateBMR();
+            });
+            assertEquals("Gender must be 'male' or 'female'", exception.getMessage());
+        }
+    }
+
+
