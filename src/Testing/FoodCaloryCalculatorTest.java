24af51ff390a5ccde73bc8c61b1a787fb561621c
WahidMubarrat <wahidmubarrat20@gmail.com>
2025-03-25 03:51:13 +0600

    Testcases for FoodCaloryCalculation

---
+++ src/Testing/FoodCaloryCalculatorTest.java
@@ -0,0 +1,28 @@
+package Testing;
+
+
+import Calculation.FoodCaloryCalculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FoodCaloryCalculatorTest {
+    FoodCaloryCalculator calc=new FoodCaloryCalculator();
+    @Test
+    public void testCalculateCaloriesForFriedChicken() {
+        String foodItem = "Fried Chicken";
+        double quantity = 200.0;
+        double calories = calc.calculateCalories(foodItem, quantity);
+
+        assertEquals(780.0, calories, 0.01);
+    }
+
+    @Test
+    public void testFoodItemNotFound() {
+        String foodItem = "Pizza";
+        double quantity = 100.0;
+
+        double calories = calc.calculateCalories(foodItem, quantity);
+
+        assertEquals(-1, calories);
+    }
+}
